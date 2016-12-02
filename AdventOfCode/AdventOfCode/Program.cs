82b3785ae7fe0d3d72c3cbbc8d5da038054031eb
Alexander Sääf <alexandersaaf95@gmail.com>
2016-12-03 00:45:40 +0100

    Add project and solution to task 1

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode {
+    class Program {
+        static void Main(string[] args) {
+
+            #region task1
+            Console.WriteLine("Running task 1");
+            Task1 task1 = new Task1();
+            String[] commands = Regex.Split(("L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4"), ", ");
+            int result = task1.CalculateDistanceToHQ(commands);
+            Console.WriteLine("The answer to Task 1 is " + result);
+            #endregion
+
+
+
+            // Keep the console window open
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Task1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+    class Task1 {
+
+        private enum Direction {
+            north, east, south, west
+        }
+
+        private Direction direction;
+
+        private int x_coord;
+        private int y_coord;
+
+
+        public int CalculateDistanceToHQ(String[] commands) {
+            direction = Direction.north;
+            x_coord = 0;
+            y_coord = 0;
+
+            //Check every command
+            foreach(String command in commands) {
+                if(command[0] == 'R') {
+                    Turn(true);
+                }else {
+                    Turn(false);
+                }
+
+                //Find out how many steps we want to take
+                int steps = Int32.Parse(command.Substring(1, command.Length - 1));
+
+                //Take the steps according to our direction
+                switch (direction) {
+                    case Direction.east:
+                        x_coord += steps;
+                        break;
+                    case Direction.west:
+                        x_coord -= steps;
+                        break;
+                    case Direction.north:
+                        y_coord += steps;
+                        break;
+                    case Direction.south:
+                        y_coord -= steps;
+                        break;
+                }   
+            }
+            //Calculate the final distance
+            int distance = Math.Abs(x_coord) + Math.Abs(y_coord);
+            return distance;
+        }
+
+
+        //This is ugly...
+        private void Turn(bool turnRight) {
+            if (direction == Direction.west) {
+                if (turnRight) {
+                    direction = Direction.north;
+                } else {
+                    direction = Direction.south;
+                }
+            }else if(direction == Direction.north) {
+                if (turnRight) {
+                    direction = Direction.east;
+                } else {
+                    direction = Direction.west;
+                }
+            } else if(direction == Direction.east) {
+                if (turnRight) {
+                    direction = Direction.south;
+                } else {
+                    direction = Direction.north;
+                }
+            } else if(direction == Direction.south) {
+                if (turnRight) {
+                    direction = Direction.west;
+                } else {
+                    direction = Direction.east;
+                }
+            }
+        }
+
+    }
+}
