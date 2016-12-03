f11b1f15efb7067d23194d6caa5dc541360a63bd
Alexander Sääf <alexandersaaf95@gmail.com>
2016-12-03 01:37:19 +0100

    Add solution for task 2

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace AdventOfCode {
     class Program {
@@ -13,11 +14,31 @@ namespace AdventOfCode {
             Console.WriteLine("Running task 1");
             Task1 task1 = new Task1();
             String[] commands = Regex.Split(("L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4"), ", ");
-            int result = task1.CalculateDistanceToHQ(commands);
-            Console.WriteLine("The answer to Task 1 is " + result);
+            int task1Result = task1.CalculateDistanceToHQ(commands);
+            Console.WriteLine("The answer to Task 1 is " + task1Result);
             #endregion
 
+            #region task2
+            Console.WriteLine("Running task 2");
+            Task2 task2 = new Task2();
 
+
+
+            //Open the file with the input
+            var filePath = Path.Combine(Environment.CurrentDirectory, "\\Task2Input.txt");
+            StreamReader file = new System.IO.StreamReader("Task2Input.txt");
+
+            //Read all the lines
+            List<string> instructions = new List<string>();
+            string line;
+            while((line = file.ReadLine()) != null) {
+                instructions.Add(line);
+            }
+
+            //Call task2 and print the result
+            string task2Result = task2.CalculateBathroomCode(instructions.ToArray());
+            Console.WriteLine("The result of task 2 is: " + task2Result);
+            #endregion
 
             // Keep the console window open
             Console.WriteLine("Press any key to exit.");
---
+++ AdventOfCode/AdventOfCode/Task2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+    class Task2 {
+
+        int xPos;
+        int yPos;
+
+        public string CalculateBathroomCode(String[] instructions) {
+
+            string code = "";
+
+            int[,] keypad = new int[3,3] { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
+
+           
+
+            //We start at 5
+            xPos = 1;
+            yPos = 1;
+
+            Console.WriteLine(keypad[2, yPos]);
+
+            //Decode every instruction
+            foreach (string instruction in instructions) {
+                //For every character in the instruction, apply change
+                foreach (char character in instruction) {
+                    switch (character) {
+                        case 'U':
+                            ChangeY(-1);
+                            break;
+                        case 'D':
+                            ChangeY(1);
+                            break;
+                        case 'L':
+                            ChangeX(-1);
+                            break;
+                        case 'R':
+                            ChangeX(1);
+                            break;
+                    }
+                }
+
+                code += keypad[xPos, yPos];
+            }
+
+            return code;
+        }
+
+        //Change xPos by dir if it is within the bounds
+        void ChangeX(int dir) {
+            if(dir == 1 && xPos != 2) {
+                xPos += 1;
+            }
+            if(dir == -1 && xPos != 0) {
+                xPos -= 1;
+            }
+        }
+
+        //Change xPos by dir if it is within the bounds
+        void ChangeY(int dir) {
+            if (dir == 1 && yPos != 2) {
+                yPos += 1;
+            }
+            if (dir == -1 && yPos != 0) {
+                yPos -= 1;
+            }
+        }
+
+    }
+}
